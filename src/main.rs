22987e7972d8c46b1f25e64faa3d293ed946b506
hurou927 <god.be.with.ye.fs@gmail.com>
2022-02-03 23:52:17 +0900

    format

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-use axum::{response::{ IntoResponse, Headers}, routing::{get, any}, Router, extract::{Path, RawBody}, http::{StatusCode, header::{self, HeaderName}, HeaderValue, Request, Response}, body::{Body, Bytes}};
-use tracing::Level;
+use axum::{
+    body::{Body, Bytes},
+    extract::Path,
+    http::{
+        header,
+        HeaderValue, Response, StatusCode,
+    },
+    routing::any,
+    Router,
+};
 use std::{net::SocketAddr, string::FromUtf8Error};
+use tracing::Level;
 
 #[tokio::main]
 async fn main() {
     let file_appender = tracing_appender::rolling::daily("logs", "app.log");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-   let collector = tracing_subscriber::fmt()
-       .with_writer(non_blocking)
-       .with_max_level(Level::INFO)
-       .finish();
+    let collector = tracing_subscriber::fmt()
+        .with_writer(non_blocking)
+        .with_max_level(Level::INFO)
+        .finish();
     tracing::subscriber::set_global_default(collector).unwrap();
 
     // build our application with a route
@@ -24,18 +33,21 @@ async fn main() {
         .unwrap();
 }
 
-fn decode_body(body: &Bytes) -> Result<String, FromUtf8Error>{
+fn decode_body(body: &Bytes) -> Result<String, FromUtf8Error> {
     String::from_utf8(body.to_vec())
 }
 
-async fn handler(Path(any): Path<String>, body: Bytes) ->  Response<Body> {
+async fn handler(Path(any): Path<String>, body: Bytes) -> Response<Body> {
     let b = decode_body(&body).unwrap();
     tracing::error!("path: {}, b: {}", any, b);
 
     let body = b;
     Response::builder()
         .status(StatusCode::OK)
-        .header(header::CONTENT_TYPE, HeaderValue::from_static( mime::APPLICATION_JSON.as_ref()))
+        .header(
+            header::CONTENT_TYPE,
+            HeaderValue::from_static(mime::APPLICATION_JSON.as_ref()),
+        )
         .body(Body::from(body))
         .unwrap()
 }
