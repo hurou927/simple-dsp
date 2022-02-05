cf44eecc32163e77dc62d3cbcbfba3525a4b3dd5
hurou927 <god.be.with.ye.fs@gmail.com>
2022-02-05 23:19:36 +0900

    use deserialize with

--- src/rtb_model.rs
+++ src/rtb_model.rs
@@ -1,24 +1,17 @@
-use serde::Deserialize;
-
-#[derive(Deserialize, PartialEq, Debug)]
-#[serde(untagged)]
-pub enum NativeRequestEnum {
-    #[serde(with = "serde_with::json::nested")]
-    AsString(NativeRequest),
-    AsStruct(NativeRequest),
-}
+use serde::{de, Deserialize};
 
 #[derive(Deserialize, PartialEq, Debug)]
 pub struct Video {}
 
 #[derive(Deserialize, PartialEq, Debug)]
 pub struct NativeRequest {
-    ver: String
+    ver: String,
 }
 
 #[derive(Deserialize, PartialEq, Debug)]
 pub struct Native {
-    request: NativeRequestEnum,
+    #[serde(deserialize_with = "nested_json_or_struct")]
+    request: NativeRequest,
 }
 
 #[derive(Deserialize, PartialEq, Debug)]
@@ -39,75 +32,92 @@ pub struct Request {
     imp: Vec<Imp>,
 }
 
+
+#[derive(Deserialize, PartialEq, Debug)]
+#[serde(untagged)]
+enum NativeRequestType {
+    #[serde(with = "serde_with::json::nested")]
+    AsNestedJson(NativeRequest),
+    AsStruct(NativeRequest),
+}
+
+fn nested_json_or_struct<'de, D>(deserializer: D) -> Result<NativeRequest, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    match NativeRequestType::deserialize(deserializer)? {
+        NativeRequestType::AsStruct(value) => Ok(value),
+        NativeRequestType::AsNestedJson(value) => Ok(value),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn parse(doc: &str) ->  Request {
+    fn parse(doc: &str) -> Request {
         serde_json::from_str(doc).unwrap()
     }
 
     #[test]
     fn empty_imp() {
-        let expected = Request{
+        let expected = Request {
             id: "req_id".to_owned(),
-            imp: Vec::new()
+            imp: Vec::new(),
         };
-        assert!(parse(r#"{"id":"req_id", "imp":[]}"#) ==  expected );
+        assert!(parse(r#"{"id":"req_id", "imp":[]}"#) == expected);
     }
     #[test]
     fn null_imp() {
-        let expected = Request{
+        let expected = Request {
             id: "req_id".to_owned(),
-            imp: Vec::new()
+            imp: Vec::new(),
         };
-        assert!(parse(r#"{"id":"req_id"}"#) ==  expected );
+        assert!(parse(r#"{"id":"req_id"}"#) == expected);
     }
     #[test]
     fn native_as_struct() {
-
-        let native_req = NativeRequestEnum::AsStruct(NativeRequest{
-           ver: "1.2".to_owned()
-        });
-
-        let expected = Request{
-            id: "req_id".to_owned(),
-            imp: vec![
-                Imp {
-                    id: "imp_id".to_owned(),
-                    video: None,
-                    native: Some(
-                        Native{
-                            request: native_req
-                    }),
-                    ext: None
-                }
-            ]
+        let native_req = NativeRequest {
+            ver: "1.2".to_owned(),
         };
-        assert!(parse(r#"{"id":"req_id","imp":[{"id":"imp_id","native":{"request":{"ver":"1.2"}}}]}"#) ==  expected );
+
+        let expected = Request {
+            id: "req_id".to_owned(),
+            imp: vec![Imp {
+                id: "imp_id".to_owned(),
+                video: None,
+                native: Some(Native {
+                    request: native_req,
+                }),
+                ext: None,
+            }],
+        };
+        assert!(
+            parse(r#"{"id":"req_id","imp":[{"id":"imp_id","native":{"request":{"ver":"1.2"}}}]}"#)
+                == expected
+        );
     }
     #[test]
     fn native_as_string() {
-
-        let native_req = NativeRequestEnum::AsString(NativeRequest{
-           ver: "1.2".to_owned()
-        });
-
-        let expected = Request{
-            id: "req_id".to_owned(),
-            imp: vec![
-                Imp {
-                    id: "imp_id".to_owned(),
-                    video: None,
-                    native: Some(
-                        Native{
-                            request: native_req
-                    }),
-                    ext: None
-                }
-            ]
+        let native_req = NativeRequest {
+            ver: "1.2".to_owned(),
         };
-        assert!(parse(r#"{"id":"req_id","imp":[{"id":"imp_id","native":{"request":"{\"ver\":\"1.2\"}"}}]}"#) ==  expected );
+
+        let expected = Request {
+            id: "req_id".to_owned(),
+            imp: vec![Imp {
+                id: "imp_id".to_owned(),
+                video: None,
+                native: Some(Native {
+                    request: native_req,
+                }),
+                ext: None,
+            }],
+        };
+        assert!(
+            parse(
+                r#"{"id":"req_id","imp":[{"id":"imp_id","native":{"request":"{\"ver\":\"1.2\"}"}}]}"#
+            ) == expected
+        );
     }
 }
-
